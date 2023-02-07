59e90c858b367e56125ebb0327f25b2f2f679c7c
MacbookAir <asyadnazeem@gmail.com>
2023-02-07 12:54:21 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let displayEl = document.getElementById("display-el")
 
-displayEl.addEventListener("click", function (){
-    console.log("5")
-})
+let icon = document.getElementById("icon")
+
+function darkTheme() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "darkTheme/sun.png"
+    }else{
+        icon.src = "darkTheme/moon.png"
+    }
+}
